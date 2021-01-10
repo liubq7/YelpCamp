d48ce6514db6e6d05fbedde52866716d0f32fe21
liubq7 <liubq97@gmail.com>
2021-01-10 12:15:47 +0800

    seed db

--- seeds/index.js
+++ seeds/index.js
@@ -23,7 +23,7 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
-      author: "5ff3d486a838642db40e05eb",
+      author: "5ffa6d9ab6b62826b89b5f2e",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       geometry: {
